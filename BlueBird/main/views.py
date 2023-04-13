926b5023327a7878816772b148bab463557d808c
Mateusz_Ormianek <mattormianek@gmail.com>
2023-04-13 22:07:59 +0200

    Created editing and deleting threads

--- BlueBird/main/views.py
+++ BlueBird/main/views.py
@@ -34,3 +34,22 @@ def create_room(request):
             return redirect('/')
     context = {'form': form}
     return render(request, 'room_form.html', context)
+
+def update_room(request, pk):
+    room = Room.objects.get(id = pk)
+    form = RoomForm(instance = room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance = room)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    context = {'form': form}
+    return render(request, 'room_form.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('/')
+
+    return render(request, 'delete.html', {'obj':room})
